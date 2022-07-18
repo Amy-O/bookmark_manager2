ad7f48bb3120a382c7f9fd8cb3fede27473dd630
Amy-O <amyalice88@gmail.com>
2022-07-18 10:03:24 +0100

    as far as I got

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,14 +1,14 @@
 require 'pg'
 
 class Bookmark
-  def self.create(url:)
+  def self.create(url:, title:)
     if ENV['ENVIRONMENT'] == 'test'
       connection = PG.connect(dbname: 'bookmark_manager_test')
     else
       connection = PG.connect(dbname: 'bookmark_manager')
     end
   
-    connection.exec("INSERT INTO bookmarks (url) VALUES('#{url}')")
+    connection.exec("INSERT INTO bookmarks (url, title) VALUES(#{url}', '#{title}') RETURNING id, url, title") 
   end
   
   def self.all
--- spec/features/adding_bookmark_spec.rb
+++ spec/features/adding_bookmark_spec.rb
@@ -2,6 +2,7 @@ feature 'Adding a new bookmark' do
   scenario 'A user can add a bookmark to Bookmark Manager' do
     visit('/bookmarks/new')
     fill_in('url', with: 'http://example.org')
+    fill_in('title', with: 'Test Bookmark')
     click_button('Submit')
 
     expect(page).to have_content 'http://example.org'
